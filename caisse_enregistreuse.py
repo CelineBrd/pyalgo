5929f082f5fb789ff27605cbc4aef28bd151a0bb
Celine <celine.barde31@gmail.com>
2019-04-03 16:37:10 +0200

    bon affichage des pièces

--- caisse_enregistreuse.py
+++ caisse_enregistreuse.py
@@ -1,13 +1,16 @@
 """
-@name rendu_monnaie.py
+@name caisse_enregistreuse.py
 @author Aélion
 @version 0.0.1
-A poor coins counter
+une caisse enregistreuse 
 """
 
-somme = 461
-billet100 = somme // 100
-reste100 = somme % 100
+aPayer = 500
+donne = 458
+rendu = aPayer - donne
+
+billet100 = rendu // 100
+reste100 = rendu % 100
 billet50 = reste100 // 50
 reste50 = reste100 % 50
 billet20 = reste50 // 20
@@ -18,7 +21,6 @@ billet5 = reste10 // 5
 reste5 = reste10 % 5
 piece2 = reste5 // 2
 piece1 = reste5 % 2
-
 
 listeBillet = [billet100,billet50,billet20,billet10,billet5,piece2,piece1]
 listeChaine = ["","","","","","",""]
@@ -32,14 +34,14 @@ for i in range(5) :
     else :
         listeChaine[i]= str(listeBillet[i]) + " billets de " + str(listeMonnaie[i])
 
-for i in range(5, 7):
+for i in range(5,7):
     if listeBillet[i] == 1:
         listeChaine[i]= str(listeBillet[i]) + " pièce de " + str(listeMonnaie[i])
     elif listeBillet[i] == 0:
         listeChaine[i] = ""
     else :
         listeChaine[i]= str(listeBillet[i]) + " pièces de " + str(listeMonnaie[i])
-
+    
 print("Je vous rend :")
 for i in range(7):
     if listeChaine[i] != "":
