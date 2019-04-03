83b2946fb5b374c54608f5f17aa10a0d6bf790d1
Celine <celine.barde31@gmail.com>
2019-04-03 11:44:32 +0200

    First commit

---
+++ hello.py
@@ -0,0 +1,11 @@
+"""
+    Algorithm #0.0.1
+    Declare var
+    Store some arithmetics into the var
+    Display result
+    Operands and Operators, functions
+"""
+
+resultat = 0 # Definition de la variable
+resultat = 2 + 2
+print(resultat)
